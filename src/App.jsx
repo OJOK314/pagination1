0485f2fa3a615b3f0f63cc7f222a4cc5b9d5a81c
ojok314 <ojokalfred1996@gmail.com>
2026-03-19 03:12:48 -0700

    my first commit in pagination1 file

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React, { useState,useEffect } from 'react'
+
+import './App.css'
+<style>
+
+</style>
+
+function App() {
+ 
+
+  return (
+    <>
+     <h1 className='h1'> hello word</h1>
+    </>
+  )
+}
+
+export default App
